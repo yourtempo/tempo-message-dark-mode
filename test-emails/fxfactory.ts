5e9314989557bd3e0eb1dc7a1e2e36f5dfa93d94
Leo Bernard <admin@leolabs.org>
2021-08-06 01:30:20 +0200

    add more examples

---
+++ test-emails/fxfactory.ts
@@ -0,0 +1,55 @@
+export const fxfactory = `<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>FxFactory - Mailing 554</title>
+</head>
+<body style="font-size:10pt;font-family:system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;margin:0;padding:16px;color:#222;background-color:#eee">
+
+	<div style="text-align:center;margin-bottom:8px;">
+		<img src="https://fxfactory.s3.amazonaws.com/downloads/mailings/assets/header-white.png" width="134" alt="FxFactory" style="width:134px;height:32px;" />
+	</div>
+
+	<div style="max-width:840px;margin:0 auto;background-color:#fff;border-radius:6px;box-shadow:0px 1px 2px rgba(0,0,0,0.2)">
+
+		<a href="https://store.fxfactory.com/send/l/w7BN72763xbCErH7V763gd77637A/3XXo9vEaav892y3LNEpWPJ763Q/jJuJWvb763WB2R9Zm3LOe2ug"><img style="border-top-left-radius:6px;border-top-right-radius:6px;" src="https://fxfactory.s3.amazonaws.com/downloads/mailings/554/title.jpg" width="100%" alt="Banner Titles+ for Final Cut Pro" border="0" /></a>
+
+		<div style="padding:40px 0;margin:0;text-align:center;border-radius:6px">
+
+			<div style="font-size:1.5em;font-weight:300;color:#222;text-align:center;max-width:500px;padding:0 16px;margin:0 auto;line-height:160%">
+				<a href="https://store.fxfactory.com/send/l/w7BN72763xbCErH7V763gd77637A/3XXo9vEaav892y3LNEpWPJ763Q/jJuJWvb763WB2R9Zm3LOe2ug" style="text-decoration:none;color:#cd0104;font-weight:600">Banner Titles+</a> mixes titles with art and animation elements to create unique, eye catching graphics
+			</div>
+			
+			<div style="font-size:1.2em;font-weight:300;color:#444;text-align:center;max-width:500px;padding:0 16px;margin:20px auto;line-height:160%">
+        		Available only for <span style="white-space:nowrap;font-weight:bold">Final Cut Pro</span>
+			</div>
+			
+			<div style="max-width:600px;margin: 0 auto">	
+				<a style="font-size:1.0em;font-weight:600;color:#cd0104;text-decoration:none;padding:5px 12px 6px 12px; background-color:#fff;border-radius:16px;border:1.5px solid #cd0104;white-space:nowrap;margin:8px 4px;display:inline-block;" href="https://store.fxfactory.com/send/l/w7BN72763xbCErH7V763gd77637A/xtK3yaY4AVbWORGC48za5A/jJuJWvb763WB2R9Zm3LOe2ug">Watch Demo</a>
+																								
+				<a style="font-size:1.0em;font-weight:600;color:#cd0104;text-decoration:none;padding:5px 12px 6px 12px; background-color:#fff;border-radius:16px;border:1.5px solid #cd0104;white-space:nowrap;margin:8px 4px;display:inline-block;" href="https://store.fxfactory.com/send/l/w7BN72763xbCErH7V763gd77637A/rXibCm3rYuwlJ9uD4X5k1w/jJuJWvb763WB2R9Zm3LOe2ug">Watch Tutorial</a>
+
+				<a style="font-size:1.0em;font-weight:600;color:#ffffff;text-decoration:none;padding:5px 12px 6px 12px; background-color:#cd0104;border-radius:16px;border:1.5px solid #cd0104;white-space:nowrap;margin:8px 4px;display:inline-block;" href="https://store.fxfactory.com/send/l/w7BN72763xbCErH7V763gd77637A/3XXo9vEaav892y3LNEpWPJ763Q/jJuJWvb763WB2R9Zm3LOe2ug">More Info</a>
+			</div>
+			
+		</div>
+
+	</div>
+
+	<div style="text-align:center;font-size:0.8em;color:#888;max-width:840px;margin:16px auto 32px auto;">
+
+		<p><span style="white-space:nowrap">&#169; 2021 Noise Industries, LLC.</span> <span style="white-space:nowrap">All rights reserved.</span> <span style="white-space:nowrap">46 Waltham St, Suite 210, Boston MA 02118.</span><br />
+		FxFactory, <span style="white-space:nowrap">Noise Industries</span> and the <span style="white-space:nowrap">Noise Industries logo</span> are trademarks of <span style="white-space:nowrap">Noise Industries, LLC</span> registered in the <span style="white-space:nowrap">United States</span>. Apple, <span style="white-space:nowrap">macOS</span>, <span style="white-space:nowrap">Final Cut Pro</span>, <span style="white-space:nowrap">Final Cut</span>, <span style="white-space:nowrap">Logic Pro</span> and GarageBand are trademarks of <span style="white-space:nowrap">Apple, Inc.</span>, registered in the U.S. and other countries. Adobe, <span style="white-space:nowrap">Adobe Premiere</span>, <span style="white-space:nowrap">After Effects</span>, and <span style="white-space:nowrap">Adobe Audition</span> are either registered trademarks or trademarks of Adobe Systems Incorporated in the United States and/or other countries.</p>
+
+		<p>Find us on:
+			<a href="https://store.fxfactory.com/send/l/w7BN72763xbCErH7V763gd77637A/V4WfbEftCD3eIWi5miyXxw/jJuJWvb763WB2R9Zm3LOe2ug" style="color:#333;text-decoration:none">YouTube</a> |
+			<a href="https://store.fxfactory.com/send/l/w7BN72763xbCErH7V763gd77637A/ZIEQaCsv0763WVkZfZqKl6Ug/jJuJWvb763WB2R9Zm3LOe2ug" style="color:#333;text-decoration:none">Facebook</a> |
+			<a href="https://store.fxfactory.com/send/l/w7BN72763xbCErH7V763gd77637A/5oKJ2onQd7C8khJ40MgnpA/jJuJWvb763WB2R9Zm3LOe2ug" style="color:#333;text-decoration:none">Twitter</a>
+		</p>
+
+		<p>Subscribed as <a href="mailto:[email]" style="color:#333;text-decoration:none">leo.bernard@me.com</a> | <a href="https://store.fxfactory.com/send/unsubscribe/wmM5Iee6ZzHmvH3k54892jNiDRbtEFRvrRd6lAl6UBmoA/7TVYT2KiBUejHVAm7rVocg/jJuJWvb763WB2R9Zm3LOe2ug"  style="color:#333;text-decoration:none">Unsubscribe</a></p>
+
+	</div>
+
+</body>
+</html>
+<img src="https://store.fxfactory.com/send/t/jJuJWvb763WB2R9Zm3LOe2ug/w7BN72763xbCErH7V763gd77637A" alt="" style="width:1px;height:1px;"/>`;
--- test-emails/index.ts
+++ test-emails/index.ts
@@ -1,19 +1,25 @@
+import { angellist } from "./angellist";
 import { bunq } from "./bunq";
+import { fxfactory } from "./fxfactory";
 import { github } from "./github";
 import { highlightedText } from "./highlighted-text";
 import { intercom } from "./intercom";
 import { komoot } from "./komoot";
+import { musicbutler } from "./musicbutler";
 import { netlifyNewsletter } from "./netlify-newsletter";
 import { notion } from "./notion";
 import { sentry } from "./sentry";
 import { twitterMessage } from "./twitter-message";
 
 export const emails = {
+  angellist,
   bunq,
+  fxfactory,
   github,
   highlightedText,
   intercom,
   komoot,
+  musicbutler,
   netlifyNewsletter,
   notion,
   sentry,
