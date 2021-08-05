2699aeec6f632d2547cc7056a7726a27430c0138
Leo Bernard <admin@leolabs.org>
2021-08-05 23:54:48 +0200

    build for importing as a module

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonJs from '@rollup/plugin-commonjs';
-import {terser} from 'rollup-plugin-terser';
-import typescript from '@rollup/plugin-typescript';
+import resolve from "@rollup/plugin-node-resolve";
+import commonJs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/dark-mode.ts",
   plugins: [commonJs(), resolve(), typescript(), terser()],
   output: {
-    name: 'DarkMode',
-    file: 'dist/dark-mode.iife.js',
-    format: 'iife'
-  }
+    name: "DarkMode",
+    file: "dist/dark-mode.iife.js",
+    format: "iife",
+    plugins: [terser()],
+  },
 };
